b99c8678853f70d33775e1853ab9510c032f7130
cmwuaan <minhquanrbpk@gmail.com>
2024-03-13 21:05:12 +0700

    init: redux react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="text-green-500">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
