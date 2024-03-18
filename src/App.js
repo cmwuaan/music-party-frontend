08b483a3555530d4afb85c794a356ed0d7cee43c
DungLe2983 <lequocdung2983@gmail.com>
2024-03-18 17:19:23 +0700

    feat(ui): create UI for AboutPage

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import About from './pages/About';
 import Home from './pages/Home';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { FaPlayCircle } from 'react-icons/fa';
 
 const AlbumCard = ({ album, listOfAlbum }) => {
