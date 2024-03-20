754a6d730b4387ebda803d72c1120583ec088ce5
Dang <21520683@gm.uit.edu.vn>
2024-03-20 20:57:58 +0700

    feat: room page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Home from './pages/Home';
+import Room from './pages/Room';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
+import RoomDetails from './pages/RoomDetail';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="px-2 pt-6">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Room />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path ="/room" element={<Room />}/>
+        <Route path ="/roomdetail" element={<RoomDetails />}/>  
       </Routes>
     </div>
   );
