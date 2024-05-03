292b76b7d36afa6f40128b7e2412fbf287601b0d
DungLe2983 <lequocdung2983@gmail.com>
2024-05-03 12:19:32 +0700

    fix(UI): remake responsive for Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaMusic } from 'react-icons/fa';
-
+import { GiHamburgerMenu } from 'react-icons/gi';
 function Header() {
-  return (
-    <header className="fixed top-0 left-0 flex justify-between items-center w-full mx-auto px-1 py-3 bg-[#202020] z-[999] backdrop-filter backdrop-blur-xl bg-opacity-30 border-b-[0.1px] border-gray-600">
-      <Link to="/">
-        <h1 className="text-green-500 font-bold text-xl hover:text-green-400 cursor-pointer ml-6 duration-200">
-          <FaMusic className="inline-block mr-2" />
-          Music Party
-        </h1>
-      </Link>
-      <ul className="flex gap-8 mr-10 text-base font-semibold items-center text-gray-300">
-        <Link to="/">
-          <li className="hover:text-white duration-200">Home</li>
-        </Link>
-        <Link to="/about">
-          <li className="hover:text-white duration-200">About</li>
-        </Link>
-        <Link to="/signin">
-          <li className="px-6 py-2 bg-white rounded-full text-black hover:bg-gray-200 hover:scale-105 duration-200">
-            Log in
-          </li>
-        </Link>
-      </ul>
-    </header>
-  );
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+    return (
+        <nav className='bg-[#202020] fixed w-full z-20 top-0 start-0  bg-opacity-30 border-b-[0.1px] border-gray-600 '>
+            <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 md:px-0 py-4'>
+                <Link to='/'>
+                    <h1 className='text-green-500 font-bold text-xl hover:text-green-400 cursor-pointer  duration-200'>
+                        <FaMusic className='inline-block mr-2' />
+                        Music Party
+                    </h1>
+                </Link>
+                <div className='flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse'>
+                    <button
+                        type='button'
+                        className='text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-transparent font-medium text-sm px-6 py-2 text-center rounded-2xl'
+                    >
+                        Login
+                    </button>
+                    <button
+                        data-collapse-toggle='navbar-sticky'
+                        type='button'
+                        className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-800 focus:outline-none focus:ring-1 focus:ring-gray-600 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+                        aria-controls='navbar-sticky'
+                        aria-expanded='false'
+                        onClick={toggleMobileMenu}
+                    >
+                        <span className='sr-only'>Open main menu</span>
+                        <GiHamburgerMenu />
+                    </button>
+                </div>
+                <div
+                    className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+                        isMobileMenuOpen ? 'block' : 'hidden'
+                    }`}
+                    id='navbar-sticky'
+                >
+                    <ul className='md:ml-[850px] ml-0 flex flex-col p-4 md:p-0 mt-4 font-medium border right-0 rounded-lg bg-gray-800 md:bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 border-gray-700'>
+                        <li>
+                            <a
+                                href='/'
+                                className='block py-2 px-3 text-gray-300 rounded md:hover:text-green-500 md:p-0 hover:bg-gray-600 md:hover:bg-transparent'
+                                aria-current='page'
+                            >
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href='/about'
+                                className='block py-2 px-3 text-gray-300 rounded md:hover:text-green-500 md:p-0 hover:bg-gray-600 md:hover:bg-transparent'
+                            >
+                                About
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default Header;
